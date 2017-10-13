4348703474aa34818a2c4b5d691873ab3e2b2b84
Michal Groele <michal.groele@polcode.net>
2017-10-13 12:10:58 +0200

    Fixed issue with Userable trait

--- app/Traits/Userable.php
+++ app/Traits/Userable.php
@@ -7,7 +7,11 @@ trait Userable
     public static function bootUserable()
     {
         static::creating(function ($object) {
-            $object->user_id = Auth::user()->id;
+            if ($object->isUnguarded() && $object->user_id) {
+                $user_id = $object->user_id;
+            } else {
+                $object->user_id = Auth::user()->id;
+            }
         });
     }
 }
